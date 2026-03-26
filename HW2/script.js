5aa9dd1759f3a0d588002c2beed9afe3bc387099
Aishali05 <arali20@cougarnet.uh.edu>
2026-03-25 23:51:57 -0500

    Update script.js

--- HW2/script.js
+++ HW2/script.js
@@ -1,58 +1,80 @@
 function reviewForm() {
 
     var fname = document.getElementsByName("fname")[0].value;
-   
     var mname = document.getElementsByName("mname")[0].value;
-   
     var lname = document.getElementsByName("lname")[0].value;
-
     var dob = document.getElementsByName("dob")[0].value;
 
-    var email = document.getElementById("email").value;
-    
-    var phone = document.getElementsByName("phone")[0].value;
+    var address1 = document.getElementsByName("address1")[0].value;
+    var address2 = document.getElementsByName("address2")[0].value;
+    var city = document.getElementsByName("city")[0].value;
+    var state = document.getElementsByName("state")[0].value;
+    var zip = document.getElementsByName("zip")[0].value;
 
-    var address = document.getElementById("address1").value;
-    var city = document.getElementById("city").value;
-    var state = document.getElementById("state").value;
-    var zip = document.getElementById("zip").value;
+    var email = document.getElementsByName("email")[0].value;
+    var userid = document.getElementsByName("userid")[0].value.toLowerCase();
 
-    var userid = document.getElementById("userid").value;
+    var phone = document.querySelector("input[placeholder='123-456-7890']").value;
 
-    document.getElementById("reviewName").innerHTML =
-        "Name: " + fname + " " + mname + " " + lname;
+    var password = document.getElementById("password").value;
+    var password2 = document.getElementById("password2").value;
 
-    document.getElementById("reviewDOB").innerHTML =
-        "Date of Birth: " + dob;
+    var symptoms = document.getElementById("symptoms").value;
 
-    document.getElementById("reviewEmail").innerHTML =
-        "Email: " + email;
+    // CHECKBOXES
+    var diseases = document.getElementsByName("disease");
+    var diseaseList = "";
 
-    document.getElementById("reviewPhone").innerHTML =
-        "Phone: " + phone;
-
-    document.getElementById("reviewAddress").innerHTML =
-        "Address: " + address + ", " + city + ", " + state + " " + zip;
-
-    document.getElementById("reviewUser").innerHTML =
-        "User ID: " + userid;
-
-    document.getElementById("reviewSection").style.display = "block";
-}
-
-function showValue(val) {
-    document.getElementById("value").innerHTML = val;
-}
-
-function checkPassword() {
-
-    var p1 = document.getElementById("password").value;
-    var p2 = document.getElementById("password2").value;
-
-    if (p1 != p2) {
-        alert("Passwords do not match");
-        return false;
+    for (var i = 0; i < diseases.length; i++) {
+        if (diseases[i].checked) {
+            diseaseList += diseases[i].value + ", ";
+        }
     }
 
-    return true;
+    // RADIO BUTTONS
+    var gender = document.querySelector('input[name="gender"]:checked');
+    var vaccinated = document.querySelector('input[name="vaccinated"]:checked');
+    var insurance = document.querySelector('input[name="insurance"]:checked');
+
+    // PASSWORD CHECK
+    var passMessage = "PASS";
+    if (password !== password2) {
+        passMessage = "ERROR: Passwords do not match";
+    }
+
+    // DOB CHECK (simple)
+    var today = new Date();
+    var birthDate = new Date(dob);
+    var dobMessage = "PASS";
+
+    if (birthDate > today) {
+        dobMessage = "ERROR: Cannot be in the future";
+    }
+
+    // OUTPUT
+    var output = `
+    Name: ${fname} ${mname} ${lname} <br>
+    DOB: ${dob} (${dobMessage}) <br><br>
+
+    Email: ${email} <br>
+    Phone: ${phone} <br><br>
+
+    Address: ${address1} ${address2}, ${city}, ${state}, ${zip} <br><br>
+
+    Diseases: ${diseaseList} <br>
+    Gender: ${gender ? gender.value : ""} <br>
+    Vaccinated: ${vaccinated ? vaccinated.value : ""} <br>
+    Insurance: ${insurance ? insurance.value : ""} <br><br>
+
+    Symptoms: ${symptoms} <br><br>
+
+    User ID: ${userid} <br>
+    Password Check: ${passMessage}
+    `;
+
+    document.getElementById("reviewOutput").innerHTML = output;
+}
+
+function updateSlider(val) {
+    document.getElementById("sliderValue").innerText = val;
 }
