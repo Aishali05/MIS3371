3ad6b4b8660c77faeef6a1ad862f5dd68691b713
Aishali05 <arali20@cougarnet.uh.edu>
2026-03-26 23:14:17 -0500

    Update script.js

--- HW2/script.js
+++ HW2/script.js
@@ -1,3 +1,11 @@
+//Program Name: JavaScript Review Form for Medical Form
+//Author: Aish Ali
+//Date Created: 03/16/2026
+//Date Last Edited: 03/26/2026
+//Version 1.0
+//Description: Review Form code to check all information on the Medical Form
+
+
 // REVIEW BUTTON FUNCTION
 function reviewForm() {
 
